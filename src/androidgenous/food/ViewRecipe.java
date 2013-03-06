01e7f2bcfec6fd8a322a9403e21c7ba55533a613
gongal <gongal@ualberta.ca>
2013-03-06 14:51:45 -0700

    Basic Activity(BUTTONS and TEXT VIEWS done) Please comment on what should be added. I havent done the listview for ingredients and recipes

---
+++ src/androidgenous/food/ViewRecipe.java
@@ -0,0 +1,22 @@
+package androidgenous.food;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewRecipe extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_view_recipe);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.view_recipe, menu);
+    return true;
+  }
+
+}
