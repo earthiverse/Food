3d280e8657c0ecd0a0ccd20d6d4ab3d121998d7c
Gongal <Gongal@uwsclient-146-56.uws.ualberta.ca>
2013-03-26 16:25:44 -0600

    Updated URL format

--- src/com/github/cmput301w13t04/food/ActivityShare.java
+++ src/com/github/cmput301w13t04/food/ActivityShare.java
@@ -52,7 +52,7 @@ public class ActivityShare extends Activity {
 				
 			}
 		}
-		String link_val = "food://com.github.cmput301w13t04.food.ActivityViewRecipe?X=1337";
+		String link_val = "food://1337";
 		
 		String link = "<a href=\"" + link_val + "\">" + "LINK TO APP ReasonableRecipe"+ "</a>";
 		body.append(link);
