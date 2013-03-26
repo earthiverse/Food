a4f25e62493f1c796bd696713ac9e782472d4abd
Christopher Solinas <solinas@ualberta.ca>
2013-03-26 14:49:16 -0600

    Photos displayed in ingredient list.

--- src/com/github/cmput301w13t04/food/ActivityManageIngredient.java
+++ src/com/github/cmput301w13t04/food/ActivityManageIngredient.java
@@ -2,6 +2,7 @@ package com.github.cmput301w13t04.food;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -16,12 +17,16 @@ import android.widget.Toast;
  */
 public class ActivityManageIngredient extends Activity {
 
+	private static final int TAKE_PHOTO = 1;
+	private Photo p;
 	int id;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manage_ingredient);
+		
+		p = null;
 
 		populate();
 	}
@@ -76,39 +81,38 @@ public class ActivityManageIngredient extends Activity {
 		// Get Name
 		EditText name = (EditText) findViewById(R.id.add_name);
 		String nameIngredient = name.getText().toString();
-		
-		if(nameIngredient.isEmpty()) {
-			Toast.makeText(view.getContext(),
-					"Missing Ingredient Name!", Toast.LENGTH_SHORT)
-					.show();
+
+		if (nameIngredient.isEmpty()) {
+			Toast.makeText(view.getContext(), "Missing Ingredient Name!",
+					Toast.LENGTH_SHORT).show();
 			return;
 		}
-		
+
 		// Get Quantity
 		EditText quantity = (EditText) findViewById(R.id.add_quantity);
 		String quantityIngredient = quantity.getText().toString();
-		
-		if(quantityIngredient.isEmpty()) {
-			Toast.makeText(view.getContext(),
-					"Missing Ingredient Quantity!", Toast.LENGTH_SHORT)
-					.show();
+
+		if (quantityIngredient.isEmpty()) {
+			Toast.makeText(view.getContext(), "Missing Ingredient Quantity!",
+					Toast.LENGTH_SHORT).show();
 			return;
 		}
-		
+
 		// Get Description
 		EditText description = (EditText) findViewById(R.id.add_description);
 		String descriptionIngredient = description.getText().toString();
-		
+
 		// Construct ingredient
 		Ingredient ingredient = new Ingredient(nameIngredient,
-				quantityIngredient, descriptionIngredient, null);
+				quantityIngredient, descriptionIngredient, p);
+
 		
 		// TODO: Add photo changes
-		
+
 		Cache cache = new Cache();
 		cache.load(view.getContext());
-		
-		if(getIntent().getBooleanExtra("INGREDIENT_EDIT", false)) {
+
+		if (getIntent().getBooleanExtra("INGREDIENT_EDIT", false)) {
 			// Update ingredient
 			cache.updateIngredient(ingredient, this.id);
 		} else {
@@ -117,12 +121,29 @@ public class ActivityManageIngredient extends Activity {
 		}
 
 		cache.save(view.getContext());
-		
-		Toast.makeText(view.getContext(),
-				"Added " + nameIngredient + "!", Toast.LENGTH_SHORT)
-				.show();
-		
+
+		Toast.makeText(view.getContext(), "Added " + nameIngredient + "!",
+				Toast.LENGTH_SHORT).show();
+
 		finish();
+	}
+
+	public void takePhoto(View view) {
+		Intent intent = new Intent(this, PhotoIntentActivity.class);
+		startActivityForResult(intent, TAKE_PHOTO);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode == TAKE_PHOTO) {
+			if (resultCode == RESULT_OK) {
+				String path = data.getStringExtra("path");
+				String desc = data.getStringExtra("desc");
+				// get User instead of null
+				p = new Photo(path, desc, null);
+			}
+		}
 	}
 
 }
