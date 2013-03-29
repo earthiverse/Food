c539c3c0d829026ad2c99b4a0cad16cc5ea51a1d
Riley <Riley@142.244.212.171>
2013-03-28 21:20:16 -0600

    Post to imgur and Fetch from url are complete, the format of the fetch return may need to be adjusted to match image viewing requirements but it's easy to change.

--- src/com/github/cmput301w13t04/food/imgurController.java
+++ src/com/github/cmput301w13t04/food/imgurController.java
@@ -1,8 +1,14 @@
 package com.github.cmput301w13t04.food;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -21,27 +27,36 @@ import android.graphics.BitmapFactory;
 import android.net.ParseException;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 
 /* 
- * NOTE: A lot of this code is taken from:
+ * NOTE: A lot of the code for imgur posting is taken from:
  *  http://stackoverflow.com/questions/7124484/android-uploading-a-photo-to-host-on-imgur-programatically
+ *  
+ *  Code for fetch:
+ *  http://stackoverflow.com/questions/4875114/android-save-image-from-url-onto-sd-card
  */
 /* To implement, do something like this:
  * 
- * Log.d("REALP",path);
-		imgurController ic = new imgurController(path);
-		try
-		{
-			String result = 
-					ic.post();
-			Log.d("YATTA", result);
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+ *Log.d("REALP",path);
+			imgurController ic = new imgurController(path);
+			String result = null;
+			try
+			{
+	//To post an image
+				result = 
+						ic.post();
+				Log.d("YATTA", result);
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	//To fetch an image.
+			String fpath = ic.fetch(result);
+			Log.d("imgur fetch result", fpath);
  * Where "path" is the string path of the photo.
  */
 public class imgurController
@@ -147,11 +162,62 @@ public class imgurController
 	public String fetch (String url){
 		String path=null;
 		FetchImageTask runner = new FetchImageTask();
+		try {
+			path = runner.execute(url).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return path;
 	}
 	private class FetchImageTask extends AsyncTask<String, Void, String> {
-	     protected String doInBackground(String... url) {
+	     protected String doInBackground(String... location) {
 	    	 String result = null;
+	    	 try
+	    	 {   
+	    	   URL url = new URL(location[0]);
+	    	   HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+	    	   urlConnection.setRequestMethod("GET");
+	    	   urlConnection.setDoOutput(true);                   
+	    	   urlConnection.connect();                  
+	    	   File SDCardRoot = Environment.getExternalStorageDirectory().getAbsoluteFile();
+	    	   String filename= location[0].substring(19);
+	    	   Log.i("Local filename:",""+filename);
+	    	   File file = new File(SDCardRoot,filename);
+	    	   if(file.createNewFile())
+	    	   {
+	    	     file.createNewFile();
+	    	   }                 
+	    	   FileOutputStream fileOutput = new FileOutputStream(file);
+	    	   InputStream inputStream = urlConnection.getInputStream();
+	    	   int totalSize = urlConnection.getContentLength();
+	    	   int downloadedSize = 0;   
+	    	   byte[] buffer = new byte[1024];
+	    	   int bufferLength = 0;
+	    	   while ( (bufferLength = inputStream.read(buffer)) > 0 ) 
+	    	   {                 
+	    	     fileOutput.write(buffer, 0, bufferLength);                  
+	    	     downloadedSize += bufferLength;                 
+	    	     Log.i("Progress:","downloadedSize:"+downloadedSize+"totalSize:"+ totalSize) ;
+	    	   }             
+	    	   fileOutput.close();
+	    	   if(downloadedSize==totalSize) result=file.getPath();    
+	    	 } 
+	    	 catch (MalformedURLException e) 
+	    	 {
+	    	   e.printStackTrace();
+	    	 } 
+	    	 catch (IOException e)
+	    	 {
+	    	   result=null;
+	    	   e.printStackTrace();
+	    	 }
+	    	 Log.i("filepath:"," "+result) ;
+
+	    	 
 	    	 return result;
 	     }
 	}
